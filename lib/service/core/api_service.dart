d60149e23d04518ec631504e5abc916f560f2129
Codesait <melasin4@gmail.com>
2022-12-02 12:21:06 +0100

    new packages for internet checking

--- lib/service/core/api_service.dart
+++ lib/service/core/api_service.dart
@@ -1,3 +1,7 @@
+import 'package:wi_weather_app/src/utils.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService{
-  
+  final log = getLogger('Logger');
+  final client = http.Client();
 }
